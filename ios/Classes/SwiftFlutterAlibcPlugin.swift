f17c7cd8755177ac750017445921e1d9145f1693
无星 <32910694@qq.com>
2021-06-01 21:17:09 +0800

    格式化

--- ios/Classes/SwiftFlutterAlibcPlugin.swift
+++ ios/Classes/SwiftFlutterAlibcPlugin.swift
@@ -3,46 +3,48 @@ import UIKit
 
 public class SwiftFlutterAlibcPlugin: NSObject, FlutterPlugin {
     
-  var alibchandler : FlutterAlibcHandle? = nil;
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_alibc", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterAlibcPlugin()
-    instance.alibchandler = FlutterAlibcHandle(channel: channel)
-    
-    registrar.addMethodCallDelegate(instance, channel: channel)
-    registrar.addApplicationDelegate(instance)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    // result("iOS " + UIDevice.current.systemVersion)
-    if call.method == "getPlatformVersion"{
-        result("iOS " + UIDevice.current.systemVersion)
-    } else if call.method == "initAlibc"{
-        alibchandler?.initAlibc(call: call, result: result)
-    } else if call.method == "loginTaoBao" {
-        alibchandler?.loginTaoBao(call: call, result: result)
-    } else if call.method == "taoKeLogin" {
-        alibchandler?.openByAsyncWebView(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcTaokeLogin.rawValue)
-    } else if call.method == "loginOut" {
-        alibchandler?.loginOut(call: call, result: result)
-    } else if call.method == "taoKeLoginForCode" {
-        alibchandler?.openByAsyncWebView(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcTaokeLoginForCode.rawValue)
-    } else if call.method == "openByUrl" {
-        alibchandler?.openByUrl(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenURL.rawValue)
-    } else if call.method == "openItemDetail"{
-        alibchandler?.openItemDetail(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenDetail.rawValue)
-    } else if call.method == "openShop"{
-        alibchandler?.openShop(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenShop.rawValue)
-    } else if call.method == "openCart"{
-        alibchandler?.openCart(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenCar.rawValue)
-    } else if call.method == "syncForTaoke"{
-        alibchandler?.syncForTaoke(call: call, result: result)
-    } else if call.method == "useAlipayNative"{
-        alibchandler?.useAlipayNative(call: call, result: result)
+    var alibchandler : FlutterAlibcHandle? = nil;
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_alibc", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterAlibcPlugin()
+        instance.alibchandler = FlutterAlibcHandle(channel: channel)
+        
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addApplicationDelegate(instance)
     }
-  }
-
-  //    MARK:- 百川处理应用跳转
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        // result("iOS " + UIDevice.current.systemVersion)
+        if call.method == "getPlatformVersion"{
+            result("iOS " + UIDevice.current.systemVersion)
+        } else if call.method == "initAlibc"{
+            alibchandler?.initAlibc(call: call, result: result)
+        } else if call.method == "loginTaoBao" {
+            alibchandler?.loginTaoBao(call: call, result: result)
+        } else if call.method == "taoKeLogin" {
+            alibchandler?.openByAsyncWebView(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcTaokeLogin.rawValue)
+        } else if call.method == "loginOut" {
+            alibchandler?.loginOut(call: call, result: result)
+        } else if call.method == "taoKeLoginForCode" {
+            alibchandler?.openByAsyncWebView(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcTaokeLoginForCode.rawValue)
+        } else if call.method == "openByUrl" {
+            alibchandler?.openByUrl(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenURL.rawValue)
+        } else if call.method == "openItemDetail"{
+            alibchandler?.openItemDetail(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenDetail.rawValue)
+        } else if call.method == "openShop"{
+            alibchandler?.openShop(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenShop.rawValue)
+        } else if call.method == "openCart"{
+            alibchandler?.openCart(call: call, result: result, callBackString: FlutterAlibcConstKey.CallBackString.AlibcOpenCar.rawValue)
+        } else if call.method == "syncForTaoke"{
+            alibchandler?.syncForTaoke(call: call, result: result)
+        } else if call.method == "useAlipayNative"{
+            alibchandler?.useAlipayNative(call: call, result: result)
+        }else{
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    //    MARK:- 百川处理应用跳转
     public func application(_ application: UIApplication, open url: URL, sourceApplication: String, annotation: Any) -> Bool {
         if !(AlibcTradeSDK.sharedInstance()?.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation))! {
             return true
@@ -57,17 +59,17 @@ public class SwiftFlutterAlibcPlugin: NSObject, FlutterPlugin {
                 op[key.rawValue] = value
             }
             
-//          转换
+            //          转换
             let isHandledByALBBSDK : Bool = (AlibcTradeSDK.sharedInstance()?.application(application, open: url, options: op))!;
             
-//            let isHandledByALBBSDK : Bool = (AlibcTradeSDK.sharedInstance()?.application(application, open: url, options:[:]))!
+            //            let isHandledByALBBSDK : Bool = (AlibcTradeSDK.sharedInstance()?.application(application, open: url, options:[:]))!
             
             return isHandledByALBBSDK
         } else {
             // Fallback on earlier versions
             //处理其他app跳转到自己的app，如果百川处理过会返回YES
         }
-
+        
         return false
     }
 }
